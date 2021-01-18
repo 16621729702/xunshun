3fde81c21c02afa9fd8074ae48f7fd6dccd72bae
xiexxin19981015 <13013171714@163.com>
2021-01-18 16:11:54 +0800

    页面显示，和轮播图显示

--- livemall-orders/src/main/java/com/wink/livemall/order/dao/LmOrderGoodsDao.java
+++ livemall-orders/src/main/java/com/wink/livemall/order/dao/LmOrderGoodsDao.java
@@ -13,10 +13,14 @@ public interface LmOrderGoodsDao extends tk.mybatis.mapper.common.Mapper<LmOrder
 
     @Select("select * from lm_order_goods where orderid=#{id} limit 0,1 ")
     LmOrderGoods findByOrderid(@Param("id") int id);
-    
+
+
+    //普通拍卖
     @Select("select * from lm_order_goods where goodid=#{id} goodstype =0  limit 1 ")
     LmOrderGoods findByGoodsid0(@Param("id") int id);
 
+
+    //直播拍卖
     @Select("select * from lm_order_goods where goodid=#{id} and goodstype =1 limit 1 ")
     LmOrderGoods findByGoodsid1(@Param("id") int id);
 
